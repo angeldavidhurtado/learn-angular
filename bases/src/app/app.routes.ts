ca13dfd38da5bfa0bfe16630f1354419df9028c2
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-10-22 21:47:41 -0500

    for

--- bases/src/app/app.routes.ts
+++ bases/src/app/app.routes.ts
@@ -19,6 +19,10 @@ export const routes: Routes = [
   },
   {
     path: 'dragonball',
-    component: DragonballComponent
-  }
+    component: DragonballComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ]
--- bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,6 +1,18 @@
-import { Component } from '@angular/core'
+interface Character {
+  id: number
+  name: string
+  power: number
+}
+
+import { Component, signal } from '@angular/core'
 
 @Component({
   templateUrl: './dragonball-page.component.html'
 })
-export class DragonballComponent {}
+export class DragonballComponent {
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegetta', power: 777 },
+    { id: 3, name: 'Trunks', power: 227 },
+  ])
+}
