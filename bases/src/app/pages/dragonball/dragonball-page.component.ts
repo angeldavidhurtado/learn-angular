2c82a4703d611810b437addb0d6bcefcd7cea586
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-10-24 20:55:44 -0500

    signal update

--- bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -10,12 +10,54 @@ import { Component, signal } from '@angular/core'
   templateUrl: './dragonball-page.component.html'
 })
 export class DragonballComponent {
+  name = signal('Gohan')
+  power = signal(100)
+
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegetta', power: 777 },
     { id: 3, name: 'Trunks', power: 227 },
     { id: 4, name: 'Yamcha', power: 500 },
   ])
+
+  /*
+  // myFunction = (e: Event & { target: HTMLInputElement }) => {
+  myFunction = (e: Event, asdf: string) => {
+    const inputElement = e.target as HTMLInputElement
+    console.log(e)
+    console.log(asdf)
+  }
+
+  myFunction = (e: HTMLInputElement, asdf: string) => {
+    console.log(e)
+    console.log(asdf)
+  }
+  */
+
+  addCharacter = () => {
+    if (!this.name() || !this.power() || this.power() <= 0)
+      return
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    }
+
+    // forma NO recomendada de actualizar el signal
+    // this.characters().push(character)
+
+    // forma RECOMENDADA de actualizra el signal
+    this.characters.update((list) => [...list, newCharacter])
+
+    this.resetFields()
+  }
+
+
+  resetFields() {
+    this.name.set('')
+    this.power.set(0)
+  }
 }
 
-// https://www.youtube.com/watch?v=f2kA2gAHpNY&list=PLCKuOXG0bPi3cfoQcSTaGUnqZbzLA30Hi&index=22
+// https://www.youtube.com/watch?v=9rkpGkxmfPQ&list=PLCKuOXG0bPi3cfoQcSTaGUnqZbzLA30Hi&index=23
