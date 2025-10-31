b759f71381f5a6a90d0eb30bc8c2dbdd1ce1fbd8
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-10-30 20:41:18 -0500

    effect

--- bases/src/app/services/dragonball.service.ts
+++ bases/src/app/services/dragonball.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from "@angular/core";
+import { effect, Injectable, signal } from "@angular/core";
 import type { Character } from "../interfaces/character.interface";
 
 @Injectable({providedIn: 'root'})
@@ -8,7 +8,13 @@ export class DragonballService {
     { id: crypto.randomUUID(), name: 'Vegetta', power: 777 },
   ])
 
+  saveToLocalStorage = effect(() => {
+    localStorage.setItem('characters', JSON.stringify(this.characters()))
+  })
+
   addCharacter = (newCharacter: Character) => {
     this.characters.update(list => [...list, newCharacter])
   }
 }
+
+// https://www.youtube.com/watch?v=4i5vZnWiNt0&list=PLCKuOXG0bPi3cfoQcSTaGUnqZbzLA30Hi&index=30
