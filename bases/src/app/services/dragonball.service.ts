54913e4c54a3b307827537dcdf93aa2c6c4cd92f
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-10-31 22:31:06 -0500

    local storaga

--- bases/src/app/services/dragonball.service.ts
+++ bases/src/app/services/dragonball.service.ts
@@ -1,12 +1,22 @@
 import { effect, Injectable, signal } from "@angular/core";
 import type { Character } from "../interfaces/character.interface";
 
+function loadFromLocalStorage(): Character[] {
+  const characters = localStorage.getItem('characters')
+  // aquí debería haber un guardia de tipo que valide que efectivamente
+  // el characters sea de tipo Characters
+  return characters ? JSON.parse(characters) : []
+}
+
 @Injectable({providedIn: 'root'})
 export class DragonballService {
+  characters = signal<Character[]>(loadFromLocalStorage())
+  /*
   characters = signal<Character[]>([
     { id: crypto.randomUUID(), name: 'Goku', power: 9001 },
     { id: crypto.randomUUID(), name: 'Vegetta', power: 777 },
   ])
+  */
 
   saveToLocalStorage = effect(() => {
     localStorage.setItem('characters', JSON.stringify(this.characters()))
@@ -16,5 +26,3 @@ export class DragonballService {
     this.characters.update(list => [...list, newCharacter])
   }
 }
-
-// https://www.youtube.com/watch?v=4i5vZnWiNt0&list=PLCKuOXG0bPi3cfoQcSTaGUnqZbzLA30Hi&index=30
